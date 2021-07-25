afc17cb987f2cc8c64454694851bab6cbd72c510
Milind Yadav <yadavmilind08@gmail.com>
2021-07-25 22:54:16 +0530

    created first 2 pages

---
+++ src/components/product-list/product-list.js
@@ -0,0 +1,58 @@
+import { Table } from 'antd';
+
+const columns = [
+    {
+        title: 'Index',
+        dataIndex: 'index',
+        render: (value, data, index) => index + 1,
+    },
+    {
+        title: 'Name',
+        dataIndex: 'name',
+    },
+    {
+        title: 'Code',
+        dataIndex: 'code',
+    },
+    {
+        title: 'Availability',
+        dataIndex: 'availability',
+        render: (availability) => availability ? 'True' : 'False',
+    },
+    {
+        title: 'Need to Repair',
+        dataIndex: 'needing_repair',
+        render: (repair) => repair ? 'True' : 'False',
+    },
+    {
+        title: 'Durability',
+        dataIndex: 'max_durability',
+    },
+    {
+        title: 'Mileage',
+        dataIndex: 'mileage',
+    },
+];
+
+const rowSelection = {
+    onChange: (selectedRowKeys, selectedRows) => {
+        console.log(`selectedRowKeys: ${selectedRowKeys}`, 'selectedRows: ', selectedRows);
+    },
+};
+
+const ProductList = ({ data }) => {
+    return (
+        <Table
+            rowSelection={{
+                type: 'radio',
+                ...rowSelection,
+            }}
+            bordered
+            columns={columns}
+            dataSource={data}
+            pagination={{ position: ['none', 'none'], pageSize: '20' }}
+        />
+    );
+}
+
+export default ProductList;
